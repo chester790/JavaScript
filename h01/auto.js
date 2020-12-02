a8671ea9d514b42a2ca3a82e3b1a15c5d2b746d2
chester790 <cbornhijm@roc-dev.com>
2020-12-02 08:45:08 +0100

    Done with Chapter 1

---
+++ h01/auto.js
@@ -0,0 +1,11 @@
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "blauw"
+}
+
+console.log(mijnauto.merk)
+console.log(mijnauto.type)
+console.log(mijnauto.aantalWielen)
+console.log(mijnauto.kleur)
